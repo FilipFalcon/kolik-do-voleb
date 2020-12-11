e7275d7c2282752909f7e8ba677a36a37afcab88
Filip Falcon <filipfalcon99@gmail.com>
2020-12-11 16:36:32 +0100

    Init commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import './lmao.js';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p id="demo"></p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
